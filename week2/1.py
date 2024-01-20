96a713efad086dc9b9207f884e200a004739be98
Sunkarbek <demohozainnn@gmail.com>
2024-01-20 17:36:51 +0600

    'first'

---
+++ week2/1.py
@@ -0,0 +1,24 @@
+txt ="Hello world"
+x= txt[2:5]
+print (x)
+
+x = "Hello world"
+print(len(x))
+
+txt ="Hello world"
+x=txt[0]
+print (x)
+
+txt ="Hello world"
+txt=txt.upper()
+print(txt)
+
+txt="Hello world"
+txt=txt.lower()
+print(txt)
+
+
+age = 36
+txt = "My name is John, and I am {}"
+
+print(txt.format(age))
