e35332d97e7d87bd4588ddf1c91f1f0c52c60735
Sunkarbek <demohozainnn@gmail.com>
2024-01-22 14:21:42 +0600

    Data types

---
+++ week2/4.py
@@ -0,0 +1,18 @@
+x = 5
+print(type(x))
+
+
+x = "Hello World"
+print(type(x))
+
+
+x = 20.5
+print(type(x))
+
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+
+x = ("apple", "banana", "cherry")
+print(type(x))
