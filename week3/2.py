c373b5d14590f8c2d81794704aae2097900a9132
Sunkarbek <demohozainnn@gmail.com>
2024-01-22 15:16:19 +0600

    list

---
+++ week3/2.py
@@ -0,0 +1,34 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0]="kiwi"
+print (fruits)
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+print(fruits)
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+print(fruits)
+
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(len(fruits))
