1ff9e91d91eea0e485d70d18cf69b7931b1f2b03
Sunkarbek <demohozainnn@gmail.com>
2024-01-29 21:57:52 +0600

    Dictionaries

---
+++ week3/5.py
@@ -0,0 +1,22 @@
+fruits = {"apple", "banana", "cherry"}
+if "apple"  in fruits:
+  print("Yes, apple is a fruit!")
+
+
+
+fruits = {"apple","kiwi","strawberry", "banana", "cherry"}
+fruits.add("orange")
+print(fruits)
+
+
+
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+print(fruits)
+
+
+
+fruits = {"apple","kiwi","strawberry", "banana", "cherry"}
+fruits.remove("banana")
+print(fruits)
