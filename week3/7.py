eacb3d3d907d4169c0c37a08c91eb3303de142e0
Sunkarbek <demohozainnn@gmail.com>
2024-01-29 22:13:50 +0600

    If else elif

---
+++ week3/7.py
@@ -0,0 +1,40 @@
+a=50
+b=30
+if a>b:
+    print("Hello world")
+
+
+a=50
+b=30
+if a!=b:
+    print("Hello world")
+
+
+a=50
+b=30
+if a==b:
+    print("Yes")
+else:
+    print("No")
+
+
+a=50
+b=30
+if a==b:
+    print("1")
+elif a>b:
+    print("2")
+else:
+    print("3")     
+
+
+
+
+a=50
+b=30
+c=20
+d=10
+if a==b and c==d:
+    print("Heloo world")
+else:
+    print("No equal")
