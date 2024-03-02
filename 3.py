6b4c3b6a398587ce8ca0b2e20562d89e489dc3a3
Sunkarbek <demohozainnn@gmail.com>
2024-03-02 17:53:04 +0600

    import module

---
+++ 3.py
@@ -0,0 +1,29 @@
+#1
+import platform
+
+x = platform.system()
+print(x)
+
+
+#2
+import platform
+
+x = dir(platform)
+print(x)
+
+
+#3
+import math
+
+print(math.factorial(10))
+
+
+#4
+import math as x
+
+print(x.factorial(3))
+
+
+
+
+
