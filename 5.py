90b7b2082bc77b7aa0389f588f121dc86e75df99
Sunkarbek <demohozainnn@gmail.com>
2024-03-02 18:20:54 +0600

    module math

---
+++ 5.py
@@ -0,0 +1,32 @@
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+
+
+#2
+x = pow(4, 3)
+
+print(x)
+
+
+
+
+
+#3
+import math
+x=math.sqrt(36)
+print(x)
+
+
+
+#4
+import math
+
+x = math.pi
+
+print(x)
