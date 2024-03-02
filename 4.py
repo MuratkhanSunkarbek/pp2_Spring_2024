bfc5eb718a879609191b9aeb7605bba11de7d6cc
Sunkarbek <demohozainnn@gmail.com>
2024-03-02 18:01:32 +0600

    datetime

---
+++ 4.py
@@ -0,0 +1,41 @@
+#1
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)
+
+
+
+
+#2
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+
+#3
+import datetime
+
+x=datetime.datetime(2024,4,2)
+print(x)
+
+
+
+#4
+import datetime
+
+x=datetime.datetime(2024,4,2)
+print(x.strftime("%B"))
+
+
+
+
+
+#5
+import datetime
+x=datetime.datetime(2024,4,2)
+print(x.strftime("%m"))
